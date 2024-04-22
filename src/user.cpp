a1b5f34172ff90d623a489c7deb67389384d25be
Raza <123810899+razaalirizvi4@users.noreply.github.com>
2024-04-22 18:36:37 +0500

    Update user.cpp
    fixed error(login returns true if any 1 of the passwords is used present in the db)

--- src/user.cpp
+++ src/user.cpp
@@ -6,30 +6,27 @@
 
 using namespace std;
 
-bool user::login(string u, string p) {
-	sqlite3* db;
-	sqlite3_stmt* stmt;
-	char* error;
-	sqlite3_open("accounts.db", &db);
-	int rc = sqlite3_exec(db, "CREATE TABLE data IF NOT EXISTS (username VARCHAR(20), password VARCHAR(20));", NULL, NULL, &error);
-	if (rc != SQLITE_OK) {
-		cout << "Error: " << error << endl;
-	}
-	string query = "SELECT * FROM data WHERE username = '";
-	query += u;
-	query += "';";
-	const unsigned char* nel = reinterpret_cast<const unsigned char*>(p.c_str());
-	rc = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
-	while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-		const unsigned char* user = sqlite3_column_text(stmt, 0);
-		const unsigned char* pass = sqlite3_column_text(stmt, 1);
-		if (*nel == *pass) {
-			cout << "Logged In Succesfully!\n";
-			return true;
-		}
-		else {
-			cout << "Incorrect Username or Password\n";
-			return false;
-		}
-	}
+bool user::loggin(string u, string p) {
+    sqlite3* db;
+    sqlite3_stmt* stmt;
+    char* error;
+    sqlite3_open("accounts.db", &db);
+    int rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS data  (username VARCHAR(20), password VARCHAR(20));", NULL, NULL, &error);
+    if (rc != SQLITE_OK) {
+        cout << "Error: " << error << endl;
+    }
+    string query = "SELECT * FROM data WHERE username = '";
+    query += u;
+    query += "';";
+    rc = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
+    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        const unsigned char* user = sqlite3_column_text(stmt, 0);
+        const unsigned char* pass = sqlite3_column_text(stmt, 1);
+        if (strcmp(reinterpret_cast<const char*>(p.c_str()), reinterpret_cast<const char*>(pass)) == 0) {
+            cout << "Logged In Successfully!\n";
+            return true;
+        }
+    }
+    cout << "Incorrect Username or Password\n";
+    return false;
 }
