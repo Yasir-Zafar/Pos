f77844730a1f32b4783dbeecc2a2ae48d35e47aa
Raza <123810899+razaalirizvi4@users.noreply.github.com>
2024-04-23 11:15:04 +0500

    Update Items.cpp + updateItem()

--- src/Items.cpp
+++ src/Items.cpp
@@ -30,3 +30,23 @@ void Items::removeItem(vector <Items> itms) {
 	itms.erase(itms.begin() + i);
 	cout << "Item Removed!\n";
 }
+void Items::updateItem(vector <Items> itms) {
+	cout << "Enter the ID of the Product to be updated: ";
+	int trgt;
+	cin >> trgt;
+	int i;
+	for (i = 0; i < itms.size(); i++) {
+		if (trgt == itms[i].id) {
+			cout << "Enter new ID: ";
+			cin >> itms[i].id;
+			cout << "Enter new name: ";
+			cin >> itms[i].name;
+			cout << "Enter new price: ";
+			cin >> itms[i].price;
+			cout << "Enter new quantity: ";
+			cin >> itms[i].quantity;
+			cout << "Product Updated!\n";
+			break;
+		}
+	}
+}
